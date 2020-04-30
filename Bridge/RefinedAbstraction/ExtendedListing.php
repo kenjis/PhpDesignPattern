1d07dcab543aadf3ca15c718173321d18a1694ae
kenjis <kenji.uui@gmail.com>
2020-04-30 20:37:15 +0900

    refactor: add argument type

--- Bridge/RefinedAbstraction/ExtendedListing.php
+++ Bridge/RefinedAbstraction/ExtendedListing.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace DoYouPhp\PhpDesignPattern\Bridge\RefinedAbstraction;
 
 use DoYouPhp\PhpDesignPattern\Bridge\Abstraction\Listing;
+use DoYouPhp\PhpDesignPattern\Bridge\Implementor\DataSource;
 
 /**
  * Listingクラスで提供されている機能を拡張する
@@ -15,7 +16,7 @@ class ExtendedListing extends Listing
     /**
      * コンストラクタ
      */
-    public function __construct($data_source)
+    public function __construct(DataSource $data_source)
     {
         parent::__construct($data_source);
     }
